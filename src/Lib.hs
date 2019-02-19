5f9d0a00c90893f47ddf42904913350f72e24268
Marek Suchánek <marek.suchanek@gmail.com>
2019-02-19 19:42:31 +0100

    Changed some tasks for B182

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,59 +1,59 @@
 module Lib
-    ( trapezoidArea
+    ( convexRegularPolygonArea
     , leapYear
-    , infoReversed
+    , infoBackwards
     , countDigits
-    , distance2D
-    , natToBinstring
-    , stringSplitToWords
-    , allType
-    , aesonAuthor
+    , manhattanDistance
+    , binboolsToNat
+    , makeMultilineString
+    , iterateType
+    , pandocAuthor
     , pluralizeFunc
     ) where
 
--- #01 = Area of trapezoid (check tests if not sure)
-trapezoidArea a b h = undefined
+-- #01 = Area of convex regular polygon (check tests if not sure, use Pi from Prelude)
+convexRegularPolygonArea n s = undefined
 
 -- #02 = Write boolean expression to check if year is leap
 leapYear :: Word -> Bool
 leapYear year = undefined
 
 -- #03 = Message about reversed string ("abc is reversed cba")
-infoReversed :: String -> String
-infoReversed str = undefined
+infoBackwards :: String -> String
+infoBackwards str = undefined
 
 -- #04 = Count number of digits of given integer
 -- (It can be done just by looking up suitable functions.
 --  Try it WITHOUT conditions and recursion!)
 countDigits x = undefined
 
--- #05 = Return euclidean distance of 2 2D points
--- (use variables x1, y1, x2, y2 in expression instead of undefined,
---  try not to use multiplication)
-distance2D (x1, y1) (x2, y2) = undefined
+-- #05 = Return Manhattan distance of 2 2D points
+-- (use variables x1, y1, x2, y2 in expression instead of undefined)
+manhattanDistance (x1, y1) (x2, y2) = undefined
 
 -- #06 = Complete the function to translate natural number to
 --       binary string (17 -> "1001", 0 -> "", 2 -> "10")
 -- (You can introduce new helper function or use prepared if-then-else
 --  by replacing True with condition, empty strings and add recursion.
 --  The if-then-else is expression as any other in Haskell, try to be DRY)
-natToBinstring :: Word -> String
-natToBinstring 0 = "" -- no need to change this, end of recursion
-natToBinstring x = if True then "" else ""
+binboolsToNat :: [Bool] -> Word
+binboolsToNat [] = 0 -- no need to change this, end of recursion
+binboolsToNat (False:bs) = undefined -- False is head of list, bs is the tail
+binboolsToNat (True:bs) = undefined -- True is head of list, bs is the tail
 
--- #07 = Lookup function that splits string to list of words (again strings)
-stringSplitToWords :: String -> [String]
-stringSplitToWords = undefined
+-- #07 = Lookup function that makes a multiline string from list of lines
+makeMultilineString :: [String] -> String
+makeMultilineString = undefined
 
--- #08 = What is the type of function "all"?
+-- #08 = What is the type of function "iterate"?
 -- (For example for "odd" it would be "Integral a => a -> Bool")
-allType :: String
-allType = "<complete here>"
+iterateType :: String
+iterateType = "<complete here>"
 
--- #09 = Who is author of "aeson" package?
+-- #09 = Who is author of "pandoc" package?
 -- (For example for "QuickCheck" it would be "Koen Claessen")
-aesonAuthor :: String
-aesonAuthor = "<complete here>"
+pandocAuthor :: String
+pandocAuthor = "<complete here>"
 
 -- #10 = You need a function that returns pluralized form of
 --       English string ("letter" -> "letters", "tooth -> teeth", ...)
--- test/Spec.hs
+++ test/Spec.hs
@@ -9,18 +9,23 @@ main :: IO ()
 main = hspec spec
 
 -- Compare floating numbers
-approxEq a b = 0.000001 > abs (a - b)
+approxEq a b = 0.0001 > abs (a - b)
 
 spec :: Spec
 spec = do
-  describe "#01 trapezoidArea" $
-    it "computes area of trapezoid" $ do
-      trapezoidArea 5 5 2 `shouldBe` 10
-      trapezoidArea 5 2 7 `shouldBe` 24.5
-      trapezoidArea 10 2 15 `shouldBe` 90
-      approxEq (trapezoidArea 7.5 6.3 2.1) 14.49 `shouldBe` True
-      trapezoidArea 2 2 0 `shouldBe` 0
-      trapezoidArea 0 0 5 `shouldBe` 0
+  describe "#01 trapezoidArea" $ do
+    it "computes area of triangle" $ do
+      approxEq (convexRegularPolygonArea 3 9) 35.0740 `shouldBe` True
+      approxEq (convexRegularPolygonArea 3 5) 10.8253 `shouldBe` True
+    it "computes area of square" $ do
+      approxEq (convexRegularPolygonArea 4 10) 100 `shouldBe` True
+      approxEq (convexRegularPolygonArea 4 2.5) 6.25 `shouldBe` True
+    it "computes area of hexagons" $ do
+      approxEq (convexRegularPolygonArea 6 66) 11317.21997 `shouldBe` True
+      approxEq (convexRegularPolygonArea 6 7) 127.30573 `shouldBe` True
+    it "computes area of other polygons" $ do
+      approxEq (convexRegularPolygonArea 10 6) 276.99151 `shouldBe` True
+      approxEq (convexRegularPolygonArea 125 1) 1243.13618 `shouldBe` True
 
   describe "#02 leapYear" $ do
     it "recognizes leap years" $ do
@@ -37,12 +42,11 @@ spec = do
       leapYear 2100 `shouldBe` False
       leapYear 2222 `shouldBe` False
 
-  describe "#03 infoReversed" $
-    it "informs about reversion" $ do
-      infoReversed "cba" `shouldBe` "abc is reversed cba"
-      infoReversed "Marek" `shouldBe` "keraM is reversed Marek"
-      infoReversed "LOL" `shouldBe` "LOL is reversed LOL"
-      infoReversed "" `shouldBe` " is reversed "
+  describe "#03 infoBackwards" $
+    it "informs about word in backwards" $ do
+      infoBackwards "cba" `shouldBe` "'abc' is 'cba' backwards"
+      infoBackwards "Marek" `shouldBe` "'keraM' is 'Marek' backwards"
+      infoBackwards "LOL" `shouldBe` "'LOL' is 'LOL' backwards"
 
   describe "#04 countDigits" $ do
     it "counts digit of positive integers" $ do
@@ -58,58 +62,54 @@ spec = do
     it "counts digit of zero" $
       countDigits 0 `shouldBe` 1
 
-  describe "#05 distance2D" $ do
-    it "computes 2D euclidean distance" $ do
-      distance2D (5, 5) (0, 0) `shouldBe` (5 * sqrt 2)
-      distance2D (1, 5) (7, 3) `shouldBe` (2 * sqrt 10)
-      distance2D (12, 3) (-1, 3) `shouldBe` 13
-      distance2D (-10, 5) (10, 15) `shouldBe` (10 * sqrt 5)
+  describe "#05 manhattanDistance" $ do
+    it "computes 2D Manhattan distance" $ do
+      manhattanDistance (5, 5) (0, 0) `shouldBe` 10
+      manhattanDistance (1, 5) (7, 3) `shouldBe` 8
+      manhattanDistance (12, 3) (-1, 3) `shouldBe` 13
+      manhattanDistance (-10, 5) (10, 15) `shouldBe` 30
     it "computes zero distance" $ do
-      distance2D (0, 0) (0, 0) `shouldBe` 0
-      distance2D (7, -2) (7, -2) `shouldBe` 0
+      manhattanDistance (0, 0) (0, 0) `shouldBe` 0
+      manhattanDistance (7, -2) (7, -2) `shouldBe` 0
     it "is commutative" $ do
-      distance2D (0, 5) (0, 3) `shouldBe` 2
-      distance2D (0, 3) (0, 5) `shouldBe` 2
+      manhattanDistance (0, 5) (0, 3) `shouldBe` 2
+      manhattanDistance (0, 3) (0, 5) `shouldBe` 2
 
-  describe "#06 natToBinstring" $ do
-    it "returns empty string for 0" $
-      natToBinstring 0 `shouldBe` ""
+  describe "#06 binboolsToNat" $ do
+    it "returns for trivials" $ do
+      binboolsToNat [] `shouldBe` 0
+      binboolsToNat [False] `shouldBe` 0
+      binboolsToNat [True] `shouldBe` 1
     it "returns valid binary string for powers of two" $ do
-      natToBinstring 1 `shouldBe` "1"
-      natToBinstring 2 `shouldBe` "10"
-      natToBinstring 16 `shouldBe` "10000"
-      natToBinstring 1024 `shouldBe` "10000000000"
-      natToBinstring 65536 `shouldBe` "10000000000000000"
+      binboolsToNat [True, False] `shouldBe` 2
+      binboolsToNat [True, False, False, False] `shouldBe` 8
+      binboolsToNat ([True] ++ replicate 10 False) `shouldBe` 1024
+      binboolsToNat ([True] ++ replicate 16 False) `shouldBe` 65536
     it "returns valid binary string for random" $ do
-      natToBinstring 15 `shouldBe` "1111"
-      natToBinstring 173 `shouldBe` "10101101"
-      natToBinstring 215 `shouldBe` "11010111"
-      natToBinstring 255 `shouldBe` "11111111"
-      natToBinstring 358 `shouldBe` "101100110"
-      natToBinstring 1279 `shouldBe` "10011111111"
+      binboolsToNat [True, False, True, True] `shouldBe` 11
+      binboolsToNat [True, True, True, True] `shouldBe` 15
+      binboolsToNat [True, False, True, False, True, True, False, True] `shouldBe` 173
+      binboolsToNat (replicate 8 True) `shouldBe` 255
+      binboolsToNat ([True, False, False] ++ replicate 8 True) `shouldBe` 1279
 
-  describe "#07 stringSplitToWords" $ do
-    it "splits by single space" $ do
-      stringSplitToWords "a b c d" `shouldBe` ["a", "b", "c", "d"]
-      stringSplitToWords "single" `shouldBe` ["single"]
-      stringSplitToWords "" `shouldBe` []
-    it "splits different whitespace chars" $ do
-      stringSplitToWords "a\nb\tc\rd" `shouldBe` ["a", "b", "c", "d"]
-      stringSplitToWords "a\r\n  b \t c \rd" `shouldBe` ["a", "b", "c", "d"]
-      stringSplitToWords "a.b\t\t\tc,d" `shouldBe` ["a.b", "c,d"]
-      stringSplitToWords "   " `shouldBe` []
+  describe "#07 makeMultilineString" $ do
+    it "joins with new line" $ do
+      makeMultilineString ["a", "b", "c", "d"] `shouldBe` "a\nb\nc\nd\n"
+      makeMultilineString ["single"] `shouldBe` "single\n"
+    it "returns empty string for no-lines" $ do
+      makeMultilineString [] `shouldBe` ""
 
-  describe "#08 allType" $ do
+  describe "#08 iterateType" $ do
     it "has length equal to 40" $
-      length allType `shouldBe` 40
+      length iterateType `shouldBe` 20
     it "should match hash" $
-      hash allType `shouldBe` 630186005456252040
+      hash iterateType `shouldBe` 3584283388811533240
 
-  describe "#09 aesonAuthor" $ do
-    it "has length equal to 16" $
-      length aesonAuthor `shouldBe` 16
+  describe "#09 pandocAuthor" $ do
+    it "has length equal to 15" $
+      length pandocAuthor `shouldBe` 15
     it "should match hash" $
-      hash aesonAuthor `shouldBe` (-4676277736506712894)
+      hash pandocAuthor `shouldBe` (-447579347104121658)
 
   describe "#10 pluralizeFunc" $ do
     it "pluralizes with s" $ do
